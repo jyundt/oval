8721dfcf792da56898823fb3079c2403e158ffca
Jacob Yundt <jyundt@gmail.com>
2016-03-21 11:22:19 -0400

    Updated models to reflect existing postgres schema (sorta).

--- app/models.py
+++ app/models.py
@@ -20,6 +20,11 @@ class RaceClass(db.Model):
     __tablename__ = 'race_class'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(200), unique=True)
+    #I'm not sure if this is how backrefs are supposed to work
+    #also, I wanted to call this 'class', but couldn't because of
+    #the reserved word
+    race_class = db.relationship('Race', backref='class')
+    
 
     def __repr__(self):
         return '<RaceClass %r>' % self.name
@@ -48,6 +53,71 @@ class Race(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, nullable=False)
     class_id = db.Column(db.Integer, db.ForeignKey('race_class.id'))
+    duration = db.Column(db.Interval)
+    slow_lap = db.Column(db.Interval)
+    fast_lap = db.Column(db.Interval)
+    average_lap = db.Column(db.Interval)
+    weather = db.Column(db.String(200))
+    usac_permit = db.Column(db.String(200))
+    laps = db.Column(db.Integer)
+
 
     def __repr__(self):
-        return '<Racer %r>' % self.name
+        return '<Race %r>' % self.name
+
+class Participant(db.Model):
+    ___tablename__ = 'participant'
+    id = db.Column(db.Integer, primary_key=True)
+    racer_id = db.Column(db.Integer, db.ForeignKey('racer.id'))
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
+    race_id = db.Column(db.Integer, db.ForeignKey('race.id'))
+
+    def __repr__(self):
+        return '<Participant %r>' % self.name
+
+
+class RaceOfficial(db.Model):
+    ___tablename__ = 'race_official'
+    id = db.Column(db.Integer, primary_key=True)
+    official_id = db.Column(db.Integer, db.ForeignKey('official.id'))
+    race_id = db.Column(db.Integer, db.ForeignKey('race.id'))
+
+    def __repr__(self):
+        return '<RaceOfficial %r>' % self.name
+
+
+class RaceMarshal(db.Model):
+    ___tablename__ = 'race_marshal'
+    id = db.Column(db.Integer, primary_key=True)
+    marshal_id = db.Column(db.Integer, db.ForeignKey('marshal.id'))
+    race_id = db.Column(db.Integer, db.ForeignKey('race.id'))
+
+    def __repr__(self):
+        return '<RaceMarshal %r>' % self.name
+
+class Prime(db.Model):
+    ___tablename__ = 'prime'
+    id = db.Column(db.Integer, primary_key=True)
+    participant_id = db.Column(db.Integer, db.ForeignKey('participant.id'))
+    description = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<Prime %r>' % self.name
+
+class Result(db.Model):
+    __tablename__ = 'result'
+    id = db.Column(db.Integer, primary_key=True)
+    participant_id = db.Column(db.Integer, db.ForeignKey('participant.id'))
+    place = db.Column(db.Integer)
+    points = db.Column(db.Integer)
+    team_points = db.Column(db.Integer)
+    mar_points = db.Column(db.Integer)
+    mar_place = db.Column(db.Integer)
+    point_prime = db.Column(db.Boolean, default=False)
+    dnf = db.Column(db.Boolean, default=False)
+    dns = db.Column(db.Boolean, default=False)
+    relegated = db.Column(db.Boolean, default=False)
+    disqualified = db.Column(db.Boolean, default=False)
+    
+    def __repr__(self):
+        return '<Result %r>' % self.name
--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from app.models import Official
+#from app.models import Official
+from app.models import *
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -12,6 +13,7 @@ migrate = Migrate(app, db)
 
 def make_shell_context():
 #    return dict(app=app, db=db, User=User, Role=Role)
+#    return dict(app=app, db=db, Official=Official)
     return dict(app=app, db=db)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
