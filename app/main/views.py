69f443ac03cd3b1c9a03a41abbfdb1c8cc4b7313
Jacob Yundt <jyundt@gmail.com>
2016-03-23 16:23:47 -0400

    Trying to figure out some queries...

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, session, redirect, url_for, current_app
+from sqlalchemy import extract
 from .. import db
 from ..models import Official,Marshal,RaceClass,Racer,Team,Race,\
     Participant,RaceOfficial,RaceMarshal,Prime,Result
@@ -7,14 +8,39 @@ from . import main
 #from .forms import NameForm
 
 
+#The goal of this function is return a table for the current standings for
+#a given season
+#e.g. Place Name Team Points
+def generate_standings(year,race_class):
+    #This is clunky, but I need to get the class_id for this race class
+    race_class_id = RaceClass.query.filter_by(description='C').first().id
+
+    #Generate a list of races for us to calculate standings
+    races = Race.query.filter(extract('year',Race.date)==2015,\
+                              Race.class_id==race_class_id).all()    
+
+    #I don't know of a better way of doing this, so I'm going to build a list 
+    # of dicts (of racers) and just do a running subtotal
+
+    rider_points = {}
+    for race in races:
+        for participant in race.participants:
+            print participant.racer.name
+            print participant.result[0].place
+            print participant.result[0].points
+           
+            
+    return races
+
 @main.route('/')
 def index():
     return render_template('index.html')
 
 @main.route('/standings')
 def standings():
+    races = generate_standings(2015,'C')
     seasons = []
     for race in Race.query.all():
         seasons.append(race.date.year)
-    #print sorted(set(seasons))
-    return render_template('standings.html', seasons=sorted(set(seasons)))
+    #return render_template('standings.html', seasons=sorted(set(seasons)))
+    return render_template('standings.html', seasons=sorted(set(races)))
