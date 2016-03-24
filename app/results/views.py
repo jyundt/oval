a4a01efcea44044cc5addb68f40e78e68afebd8e
Jacob Yundt <jyundt@gmail.com>
2016-03-23 23:29:12 -0400

    Defining initial views for Race and Racer results.

--- app/results/views.py
+++ app/results/views.py
@@ -2,10 +2,25 @@ from flask import render_template, redirect, request, url_for, flash
 from . import results
 from .. import db
 from .forms import RaceResultsForm
+from ..models import Race,Racer
 
-@results.route('/race')
-def race_results():
-    form = RaceResultsForm()
-    return render_template('results/race.html', form=form)
+@results.route('/race/<int:id>')
+def race_results(id):
+    #form = RaceResultsForm()
+    #return render_template('results/race.html', form=form)
+    race = Race.query.get_or_404(id)
+    #return '<h1>Found race for %s</h1>' % race.date
+    return render_template('results/race.html',race=race)
+    
+        
+
+@results.route('/racer/<int:id>')
+def racer_results(id):
+    #form = RaceResultsForm()
+    #return render_template('results/race.html', form=form)
+    racer = Racer.query.get_or_404(id)
+    #return '<h1>Found race for %s</h1>' % race.date
+    return render_template('results/racer.html',racer=racer)
+    
         
 
